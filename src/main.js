f90bb8a8246838533d0ecb63a648e193b5958f28
Roy <rxlisbest@gmail.com>
2018-10-08 15:29:22 +0800

    样式引入

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import { router } from './router';
 
+import 'vant/lib/vant-css/base.css';
+import 'vant/lib/vant-css/button.css';
+import 'vant/lib/vant-css/index.css';
+
 // Vue.config.productionTip = false
 
 new Vue({
