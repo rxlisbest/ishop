e3d3776f91ca8373b61b62c001e2c32567d324af
Roy <rxlisbest@gmail.com>
2018-10-19 11:27:25 +0800

    编辑 地址

--- src/view/address/add.vue
+++ src/view/address/add.vue
@@ -8,100 +8,35 @@
       @click-left="onClickLeft"
       @click-right="onClickRight"
     />
-    <van-tabs v-model="active2">
-      <van-tab title="全部"></van-tab>
-      <van-tab title="待付款"></van-tab>
-      <van-tab title="待发货"></van-tab>
-      <van-tab title="待收货"></van-tab>
-      <van-tab title="待评价"></van-tab>
-    </van-tabs>
-    <div class="index-shop">
-      <div class="index-shop-item">
-        <van-cell-group>
-          <van-cell value="Apple官方旗舰店" />
-        </van-cell-group>
-        <div class="card-goods">
-          <div
-            class="card-goods__item"
-            v-for="item in goods"
-            :key="item.id"
-            :name="item.id"
-          >
-            <van-card
-              :title="item.title"
-              :desc="item.desc"
-              :num="item.num"
-              :price="formatPrice(item.price)"
-              :thumb="item.thumb"
-            />
-          </div>
-        </div>
-        <div class="index-shop-item-total">
-          共3件商品 合计:￥100.00
-        </div>
-        <van-cell-group>
-          <van-cell value="猜你喜欢" />
-        </van-cell-group>
-      </div>
-    </div>
-    <ishop-footer :active="active"></ishop-footer>
+    <van-address-edit
+      :area-list="areaList"
+      show-postal
+      show-delete
+      show-set-default
+      show-search-result
+      :search-result="searchResult"
+      @save="onSave"
+      @delete="onDelete"
+      @change-detail="onChangeDetail"
+    />
   </div>
 </template>
 
 <script>
 import {
   NavBar,
-  CellGroup,
-  Cell,
-  Card, 
-  Tab, 
-  Tabs,
+  AddressEdit,
 } from 'vant';
 export default {
   components: {
     [NavBar.name]: NavBar,
-    [CellGroup.name]: CellGroup,
-    [Cell.name]: Cell,
-    [Card.name]: Card,
-    [Tab.name]: Tab,
-    [Tabs.name]: Tabs,
+    [AddressEdit.name]: AddressEdit,
   },
   data() {
     return {
-      active: 3,
-      active2: 0,
-      goods: [{
-        id: '1',
-        title: '进口香蕉',
-        desc: '约250g，2根',
-        price: 200,
-        num: 1,
-        thumb: 'https://img.yzcdn.cn/public_files/2017/10/24/2f9a36046449dafb8608e99990b3c205.jpeg'
-      }, {
-        id: '2',
-        title: '陕西蜜梨',
-        desc: '约600g',
-        price: 690,
-        num: 1,
-        thumb: 'https://img.yzcdn.cn/public_files/2017/10/24/f6aabd6ac5521195e01e8e89ee9fc63f.jpeg'
-      }, {
-        id: '3',
-        title: '美国伽力果',
-        desc: '约680g/3个',
-        price: 2680,
-        num: 1,
-        thumb: 'https://img.yzcdn.cn/public_files/2017/10/24/320454216bbe9e25c7651e1fa51b31fd.jpeg'
-      }]
+      areaList,
+      searchResult: []
     };
-  },
-  computed: {
-    submitBarText() {
-      const count = this.checkedGoods.length;
-      return '结算' + (count ? `(${count})` : '');
-    },
-    totalPrice() {
-      return this.goods.reduce((total, item) => total + (this.checkedGoods.indexOf(item.id) !== -1 ? item.price : 0), 0);
-    }
   },
   methods: {
     onClickLeft() {
@@ -110,9 +45,22 @@ export default {
     onClickRight() {
       Toast('按钮');
     },
-    formatPrice(price) {
-      return (price / 100).toFixed(2);
+    onSave() {
+      Toast('save');
     },
+    onDelete() {
+      Toast('delete');
+    },
+    onChangeDetail(val) {
+      if (val) {
+        this.searchResult = [{
+          name: '黄龙万科中心',
+          address: '杭州市西湖区'
+        }];
+      } else {
+        this.searchResult = [];
+      }
+    }
   }
 };
 </script>
